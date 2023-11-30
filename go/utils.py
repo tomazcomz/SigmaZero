91c6a12b672a1d232e304eb9e978fad6ebf0d58a
Gonto03 <gapmd2003@gmail.com>
2023-11-30 16:24:40 +0000

    calculating number of captured territories (almost done)

--- go/utils.py
+++ go/utils.py
@@ -26,7 +26,7 @@ def _flood_fill(i,j,original_piece,board,group_positions=set(),visited=set()):
     elif position == -original_piece:
         return False, group_positions           # this position has an opposing piece to the original position being checked
 
-    neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]      # if (i,j) has the same Piece as the original position, its neighbors will be checked
+    neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]    # if (i,j) has the same piece as the original position, its neighbors will be checked
     for i,j in neighbors:
         result, group_positions = _flood_fill(i,j,original_piece,board,group_positions,visited)
         if result:
@@ -57,22 +57,41 @@ def get_captured_territories(i,j,board):
     return ct_group, captor
 
 # recursive helper method that implements an algorithm that searches for a captured group of territories
-def _get_captured_territories(self,i,j,board,ct_group=set(),captor=0,visited=set()):
+def _get_captured_territories(i,j,board,ct_group=set(),captor=0,visited=set()):
     if (i,j) in visited or invalid_position(i,j,len(board)):
         return ct_group, captor
     visited.add((i,j))
     if board[i][j] != 0:    # if this position isn't empty, it checks whose player it belongs to
         if captor == 0:
-            captor = board[i][j]    # getting the captor of this group.
+            captor = board[i][j]     # getting the captor of this group, if there isn't one yet
             return ct_group, captor
-        elif board[i][j]!=captor:   # If there's two different captors to the group's positions, then
-            return None,0           #   it returns None, because there's no group captured by one captor
-    # if this position is empty, then ... (continue from here)
+        elif board[i][j]!=captor:    # If there's two different captors to the group's positions, then
+            return None,0   # it returns None, because there's no group captured by one captor
+        return ct_group, captor     # this piece is captured by the same captor as every piece in this group checked so far
+    ct_group.add((i,j))  # if this position is empty, then it is added to the territory group
+    neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]   # if (i,j) has the same piece as the original position, its neighbors will be checked
+    for x,y in neighbors:
+        ct_group,captor = _get_captured_territories(x,y,board,ct_group,captor,visited)
+    # (continue from here)
     
+def get_captured_territories_example():
+    board = np.array([
+        [0,-1,-1,0],
+        [-1,1,1,0],
+        [0,0,0,0],
+        [1,0,0,1]
+    ])
+    i=0
+    j=0
+    ct_group, captor = _get_captured_territories(i,j,board)
+    print(ct_group)
+    print(f"captor: {captor}")
+    
+get_captured_territories_example()
 
 
-# The point a is adjacent to a black stone. Therefore, a does not belong to White's territory. 
-# However, a is connected to b (by the path shown in the diagram, among others), which is adjacent to a white stone. 
-# Therefore, a does not belong to Black's territory either. In conclusion, a is neutral territory.
+# The point A is adjacent to a black stone. Therefore, A does not belong to White's territory. 
+# However, A is connected to B (by the path shown in the diagram, among others), which is adjacent to a white stone. 
+# Therefore, A does not belong to Black's territory either. In conclusion, A is neutral territory.
 # An empty point only belongs to somebody's territory, 
 #   if all the empty intersections that form a connected group with it are adjacent to stones of that player's territory
