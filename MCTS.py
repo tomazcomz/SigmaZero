829cd5f844fa72bc34680cb44a8e839cca5b1197
Sellani <125892880+nnsellani@users.noreply.github.com>
2023-12-21 10:27:02 +0000

    pôr mais bonitinho; continua duvidas no expand!

--- MCTS.py
+++ MCTS.py
@@ -40,16 +40,10 @@ class Node:
         return len(self.children)>0 # if no expandable moves and there are children
     
     def select(self):
-        selected=None # selected child
-        max_score=-np.inf # get the max ucb obtained
-
-        for child in self.children:
-            score=self.ucb(child)
-            if score>max_score:
-                selected=child
-                max_score=score
-        
-        return selected
+        if not self.children:
+            return None
+        selected = max(self.children, key=lambda child: self.ucb(child))
+        return selected.select()
     
     def ucb(self, child): 
         # mean_action_value Q=W/N
@@ -83,7 +77,7 @@ class MCTS:
     def play(self,state):
         root=Node(self.game_state,self.args,state)
 
-        for search in range(self.args['num_searches']):
+        for _ in range(self.args['num_searches']):
             node=root
 
             # selection
@@ -125,3 +119,4 @@ args = {
     'num_searches': 1600
 }
 
+mcts=MCTS()
