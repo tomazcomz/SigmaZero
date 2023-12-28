ec3cda431442c39b5cc5e5fdcc1d34a5e0b04c74
Sellani <125892880+nnsellani@users.noreply.github.com>
2023-12-28 22:08:35 +0000

    noise

--- MCTS.py
+++ MCTS.py
@@ -55,7 +55,7 @@ class Node:
 
     def expand(self, p):
         for _ in range(self.untried_actions.len()):
-            action = self.untried_actions.pop()
+            action = self.untried_actions.pop() # coordenada
             next_state = self.game_state.move(action[0], action[1])
             child = Node(next_state, parent=self, p_action=action, prior_prob=p)
             self.children.append(child)
@@ -68,22 +68,25 @@ class Node:
 
 class MCTS:
     def __init__(self, args,tind, model,eva=False):
-        #self.game_state=game_state
         self.args=args
         self.model=model
         self.evaluate=eva
         self.ti=tind            # ver * em ideias.md
-    
-    def play(self, state):
-        """ Confusão com states.
-        self.game_state e node.game_state
-        ou 
-        dar um state e ver por aí"""
+        self.root=None # for updating root node 
 
-        root=Node(self.game_state,self.args)
+    def get_child(self, node, action): # find child node associated with action
+        for child in node.children:
+            if child.p_action==action:
+                return child
+        return None
+    
+    def play(self):
+        if self.root is None:
+            p=0.75*self.root.prior_prob+0.25*0.03 # adding Dirichlet noise to root's prior 
+            self.root=Node(self.game_state, self.args, prior_prob=p)
 
         for _ in range(self.args['num_searches']):
-            node=root
+            node=self.root
 
             # selection
             while node.fully_expanded():
@@ -94,15 +97,8 @@ class MCTS:
 
             # expand and evaluate
             if not terminal:
-                p, v = self.model.predict(node.game_state) # to do
+                p, v = self.model.predict(node.game_state)
                 node.expand(p) # adding childs with policy from the NN to list children
-                
-                # parte incerta - qual o value que se propagará? v do nó folha
-                """children=Go.create_children(node)
-                for child in children:
-                    Then for each possible action on the new node, we add a new edge (s, a). 
-                    We initialize each edge with the visit count N, W, and Q to 0. 
-                    And we record the corresponding v and p.""" 
 
             # backpropagate
             node.backprop(v)
@@ -113,20 +109,14 @@ class MCTS:
             temp=0
 
         action_prob=np.zeros(self.game_state.n**2+1)
-        for child in root.children:
+        for child in self.root.children:
             action_prob[child.action_taken] = node.visit_count**(1/temp)/self.visit_count**(1/temp)
         max_prob_index = np.argmax(action_prob)
         if max_prob_index == self.game_state.n**2:
             return (-1, -1)     # definir isto como "pass"
         else:
-            return ((max_prob_index // self.game_state.n), (max_prob_index % self.game_state.n))    # converter indice de array 1D em coordenadas de array 2D
-
-
-# test part ----------------------------------------------------------------
-args = {
-    'cput': 10**-4,
-    'num_searches': 1600
-}
-
-mcts=MCTS(args)
+            played=((max_prob_index // self.game_state.n), (max_prob_index % self.game_state.n))    # converter indice de array 1D em coordenadas de array 2D
+            self.root=self.get_child(self.root, played) # new root node is the child corresponding to the played action
+            return played
+    
 
